8a7711e25e8291d1d6f072c24a9224cb0b55bd38
tbijewar04 <tejasbijewar5391@gmail.com>
2025-09-26 22:58:50 +0530

    Add files via upload

---
+++ tirthongo/src/main/java/com/tirthongo/Main.java
@@ -0,0 +1,21 @@
+package com.tirthongo;
+
+import com.tirthongo.view.AboutUs;
+import com.tirthongo.view.BookingConfirmPage;
+import com.tirthongo.view.DestinationPage;
+//import com.tirthongo.view.AdminProfile;
+import com.tirthongo.view.LoginPage;
+import com.tirthongo.view.Splash;
+//import com.tirthongo.view.CheckOut;
+//import com.tirthongo.view.ContactUsPage;
+//import com.tirthongo.view.MyTrips;
+//import com.tirthongo.view.DestinationPage;
+//import com.tirthongo.view.UserProfile;
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Application.launch(Splash.class, args);
+    }
+}
